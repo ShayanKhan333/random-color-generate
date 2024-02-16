9c97ad35d664ce6f500a904b5f63d514c3206ef4
ShayanKhan333 <shayan.13579.khan@gmail.com>
2024-02-16 23:42:22 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,13 @@
+let box =document.querySelector(".container").children
+
+const randonColorGenerate=()=>{
+    let val1 =Math.ceil(0+Math.random()*255)
+let val2 =Math.ceil(0+Math.random()*255)
+let val3 =Math.ceil(0+Math.random()*255)
+return`rgb(${val1},${val2},${val3})`
+
+}
+Array.from(box).forEach(e => {
+    e.style.backgroundColor=randonColorGenerate()
+});
+
